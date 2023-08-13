7ce6be42d23d532ff5ae6f78ceea69703c1b779e
Kuinie Fu <kuiniefu@outlook.com>
2023-08-13 21:36:04 +0800

    初始化交易

--- personal_expenses/lib/main.dart
+++ personal_expenses/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'transaction.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -15,15 +17,29 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Personal Expense'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  List<Transaction> _transactions = [
+    Transaction(
+        id: DateTime.now().toString(),
+        title: 'Shoes',
+        amount: 10.3,
+        date: DateTime.now()),
+    Transaction(
+        id: DateTime.now().toString(),
+        title: 'Eating',
+        amount: 80,
+        date: DateTime.now()),
+    Transaction(
+        id: DateTime.now().toString(),
+        title: 'Weakly Funny',
+        amount: 130.99,
+        date: DateTime.now()),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -32,17 +48,29 @@ class MyHomePage extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(title),
+        title: Text('Expenses'),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Text(
-              'char',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        // crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              elevation: 5,
+              color: Colors.blue,
+              child: Text(
+                'charts',
+              ),
             ),
-          ],
-        ),
+          ),
+          Column(
+              children: _transactions
+                  .map((e) => Card(
+                        child: Text(e.title),
+                      ))
+                  .toList()),
+        ],
       ),
     );
   }
