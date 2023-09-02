9d4c7de16a0ccf36f8ced043ebd333ff56fd36bf
Kuinie Fu <kuiniefu@outlook.com>
2023-09-02 12:49:37 +0800

    固定滚动列设置

--- personal_expenses/lib/widgets/transaction_list.dart
+++ personal_expenses/lib/widgets/transaction_list.dart
@@ -11,52 +11,57 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        children: transactions
-            .map((e) =>
-            Card(
-              child: Row(
-                children: [
-                  Container(
-                    margin:
-                    EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.purple,
-                        width: 2,
-                      ),
-                    ),
-                    child: Text(
-                      '\$${e.amount}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple,
-                      ),
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+            children: transactions
+                .map((e) =>
+                Card(
+                  child: Row(
                     children: [
-                      Text(
-                        '${e.title}',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
+                      Container(
+                        margin:
+                        EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.purple,
+                            width: 2,
+                          ),
+                        ),
+                        child: Text(
+                          '\$${e.amount}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          ),
                         ),
                       ),
-                      Text(
-                        DateFormat('yyyy/MM/dd').format(e.date),
-                        style: TextStyle(
-                          color: Colors.grey,
-                        ),
-                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '${e.title}',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            DateFormat('yyyy/MM/dd').format(e.date),
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      )
                     ],
-                  )
-                ],
-              ),
-            ))
-            .toList());
+                  ),
+                ))
+                .toList()),
+      ),
+    );
   }
 }
